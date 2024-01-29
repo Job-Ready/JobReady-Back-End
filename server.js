a956cf69958dc785f472d594f2344498add6340f
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2024-01-29 22:17:32 +0200

    connect to db

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 // server.js
 const express = require('express');
+const { Sequelize } = require('sequelize');
 const bodyParser = require('body-parser'); // For parsing JSON in request body
 const app = express();
 const port = 3001;
 const cors = require('cors');
 
-app.use(bodyParser.json());
 const corsOptions = {
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
 };
-  
+
+const sequelize = new Sequelize({
+    user: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    port: 5432, // default Postgres port
+    database: 'JobReady-local',
+    dialect: 'postgres',
+  })
+
+app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Hello, this is your resume builder backend!');
 });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to PostgreSQL');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // Example route to save a resume
 app.post('/saveResume', (req, res) => {
