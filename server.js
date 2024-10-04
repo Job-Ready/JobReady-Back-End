86ff584956386713f650f0fb94924dffadc2a772
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2024-10-04 14:52:18 +0200

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config({
   path: path.resolve(__dirname, ".env"),
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
 
 const server = http.createServer(app);
